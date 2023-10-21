eecf71f40d898df8a4cc3da7983ab1a5ff6b6aa8
Rajesh <115442548+rajeshd1997@users.noreply.github.com>
2023-10-21 17:02:35 +0000

    added first file

---
+++ Basics/first.java
@@ -0,0 +1,11 @@
+class Helloworld{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
+class SecHelloworld{
+    public static void main(String[] args){
+        System.out.println("Hello World in Sec");
+    }
+}
+
