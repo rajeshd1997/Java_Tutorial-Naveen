d320858a37aebd5a4338acf609bedb62775fa7a7
Rajesh <115442548+rajeshd1997@users.noreply.github.com>
2023-10-21 17:32:09 +0000

    Added Winner

--- Basics/first.java
+++ Basics/first.java
@@ -8,4 +8,12 @@ class SecHelloworld{
         System.out.println("Hello World in Sec");
     }
 }
+class Winner{
+    public static void main(String[] args)
+    {
+        System.out.println("Your the winner Today");    
+    }
+
+}
+    
 
